f3f66a21fb2974b743197aa301706d895ab1be85
Hugo Venega <hugo.venega.0@gmail.com>
2021-07-22 14:19:07 -0300

    Add res method to fraction and test it

--- src/fraction.js
+++ src/fraction.js
@@ -15,6 +15,18 @@ class Fraction {
       console.log(nNum, nDen);
     }
   }
+
+  res(fr) {
+    if (this.den !== fr.den) {
+      const nDen = this.den * fr.den;
+      const nNum = (this.num * fr.den) - (fr.num * this.den);
+      console.log(nNum, nDen);
+    } else {
+      const nDen = this.den;
+      const nNum = (this.num - fr.num);
+      console.log(nNum, nDen);
+    }
+  }
 }
 
 const fr1 = new Fraction(1, 5);
@@ -25,3 +37,7 @@ console.log('Pruebas .add con 1/5 y 1/4');
 fr1.add(fr2);
 console.log('Pruebas .add con 1/3 y 2/3');
 fr3.add(fr4);
+console.log('Pruebas .res con 1/5 y 1/4');
+fr1.res(fr2);
+console.log('Pruebas .res con 1/3 y 2/3');
+fr3.res(fr4);
