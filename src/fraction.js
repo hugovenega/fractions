48ac1be9a4b7f7c87764e8a949fb4edbbdb273f1
Hugo Venega <hugo.venega.0@gmail.com>
2021-07-22 14:54:21 -0300

    Add code enhancements

--- src/fraction.js
+++ src/fraction.js
@@ -5,59 +5,48 @@ class Fraction {
   }
 
   add(fr) {
+    let nDen;
+    let nNum;
     if (this.den !== fr.den) {
-      const nDen = this.den * fr.den;
-      const nNum = (this.num * fr.den) + (fr.num * this.den);
-      console.log(nNum, nDen);
+      nDen = this.den * fr.den;
+      nNum = (this.num * fr.den) + (fr.num * this.den);
     } else {
-      const nDen = this.den;
-      const nNum = (this.num + fr.num);
-      console.log(nNum, nDen);
+      nDen = this.den;
+      nNum = (this.num + fr.num);
     }
+    return (`${nNum} ` + ` ${nDen}`);
   }
 
   res(fr) {
+    let nDen;
+    let nNum;
     if (this.den !== fr.den) {
-      const nDen = this.den * fr.den;
-      const nNum = (this.num * fr.den) - (fr.num * this.den);
-      console.log(nNum, nDen);
+      nDen = this.den * fr.den;
+      nNum = (this.num * fr.den) - (fr.num * this.den);
     } else {
-      const nDen = this.den;
-      const nNum = (this.num - fr.num);
-      console.log(nNum, nDen);
+      nDen = this.den;
+      nNum = (this.num - fr.num);
     }
+    return (`${nNum} ` + ` ${nDen}`);
   }
 
   mul(fr) {
     const nNum = this.num * fr.num;
     const nDen = this.den * fr.den;
-    console.log(nNum, nDen);
+    return (`${nNum} ` + ` ${nDen}`);
   }
 
   div(fr) {
     const nNum = this.num * fr.den;
     const nDen = this.den * fr.num;
-    console.log(nNum, nDen);
+    return (`${nNum} ` + ` ${nDen}`);
   }
 }
 
-const fr1 = new Fraction(1, 5);
-const fr2 = new Fraction(1, 4);
-const fr3 = new Fraction(1, 3);
-const fr4 = new Fraction(2, 3);
-console.log('Pruebas .add con 1/5 y 1/4');
-fr1.add(fr2);
-console.log('Pruebas .add con 1/3 y 2/3');
-fr3.add(fr4);
-console.log('Pruebas .res con 1/5 y 1/4');
-fr1.res(fr2);
-console.log('Pruebas .res con 1/3 y 2/3');
-fr3.res(fr4);
-console.log('Pruebas .mul con 1/5 y 1/4');
-fr1.mul(fr2);
-console.log('Pruebas .mul con 1/3 y 2/3');
-fr3.mul(fr4);
-console.log('Pruebas .div con 1/5 y 1/4');
-fr1.div(fr2);
-console.log('Pruebas .div con 1/3 y 2/3');
-fr3.div(fr4);
+const a = new Fraction(1, 5);
+const b = new Fraction(1, 4);
+const c = (a.add(b));
+const d = (a.res(b));
+const e = (a.mul(b));
+const f = (a.div(b));
+console.log(c, d, e, f);
